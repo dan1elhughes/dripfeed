ba909498e11cd89d9e62413b3764bed25e68e0a2
Dan Hughes <dan.hughes@mirumagency.com>
2018-05-04 14:46:47 +0100

    Default localstorage to empty settings object

--- src/Store.js
+++ src/Store.js
@@ -4,7 +4,7 @@ const KEY = '_';
 
 export default class Store {
 	get(key) {
-		const data = JSON.parse(localStorage.getItem(KEY) || '{}');
+		const data = JSON.parse(localStorage.getItem(KEY) || '{ "settings": [] }');
 		return key ? data[key] : data;
 	}
 
