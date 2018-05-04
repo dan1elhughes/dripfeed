dd173bf4b265d829de0ed784285292ba043c0ede
Dan Hughes <dan.hughes@mirumagency.com>
2018-05-04 18:21:36 +0100

    Remove minification, add native css output

--- src/styles/_build.node.js
+++ src/styles/_build.node.js
@@ -9,16 +9,30 @@ const file = path.join(__dirname, '/tokens.yaml');
 const writeJSON = tokens =>
 	fs.writeFileSync(
 		path.join(__dirname, '/tokens.json'),
-		JSON.stringify(tokens)
+		JSON.stringify(tokens, null, 4)
 	);
 
 const writeSass = tokens => {
 	const flattened = flatten(tokens, { delimiter: '-' });
 	const output = Object.entries(flattened)
-		.map(([key, value]) => `$${key}: "${value}";`)
-		.join('');
+		.map(([key, value]) => `$${key}: '${value}';`)
+		.join('\n');
 
 	fs.writeFileSync(path.join(__dirname, '/tokens.scss'), output);
+};
+
+const writeCss = tokens => {
+	const flattened = flatten(tokens, { delimiter: '-' });
+
+	const before = ':root {\n';
+	const after = '\n}';
+
+	const content = Object.entries(flattened)
+		.map(([key, value]) => `\t--${key}: '${value}';`)
+		.join('\n');
+
+	const output = `${before}${content}${after}`;
+	fs.writeFileSync(path.join(__dirname, '/tokens.css'), output);
 };
 
 try {
@@ -26,6 +40,7 @@ try {
 	const tokens = yaml.safeLoad(content);
 	writeJSON(tokens);
 	writeSass(tokens);
+	writeCss(tokens);
 } catch (e) {
 	console.error(e); // eslint-disable-line
 }
