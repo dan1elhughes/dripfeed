30d285041c0446b015760ebdddec8f2eeced81f9
Dan Hughes <dan.hughes@mirumagency.com>
2018-06-25 16:24:20 +0100

    Minor syntax change on theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -4,12 +4,13 @@ import tokens from './tokens.flat.json';
 const themed = identifier => props => {
 	const withDark = identifier + '-dark';
 	const withLight = identifier + '-light';
-	if (!tokens[withDark]) {
+
+	if (!tokens[withDark])
 		return console.error(`${withDark} is not a defined token`);
-	}
-	if (!tokens[withLight]) {
+
+	if (!tokens[withLight])
 		return console.error(`${withLight} is not a defined token`);
-	}
+
 	return `var(--${props.theme.isDarkMode ? withDark : withLight})`;
 };
 
