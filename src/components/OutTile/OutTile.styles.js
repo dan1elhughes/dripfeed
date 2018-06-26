b955c1976f5d2ca040338176fa8d9f90f419b880
Dan Hughes <dan.hughes@mirumagency.com>
2018-06-26 12:04:32 +0100

    Better holiday UI

--- src/components/OutTile/OutTile.styles.js
+++ src/components/OutTile/OutTile.styles.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 export const StyledName = styled.h3`
-	margin-bottom: 0px;
+	margin-bottom: 12px;
 `;
 
 export const StyledSubText = styled.p`
 	margin-top: 0px;
+	margin-bottom: 0px;
 	text-transform: uppercase;
 	color: var(--color-text-dim);
 `;
